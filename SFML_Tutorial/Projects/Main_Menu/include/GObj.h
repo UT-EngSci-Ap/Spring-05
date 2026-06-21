f31cb81fa823f57ce2c63116e89e08120c398998
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-22 02:19:00 +0330

    edit GObj class

--- SFML_Tutorial/Projects/Main_Menu/include/GObj.h
+++ SFML_Tutorial/Projects/Main_Menu/include/GObj.h
@@ -11,6 +11,10 @@ public:
     virtual ~GObj() = default;
     GObj(const sf::Texture &t) : sprite(t) {}
     void draw(sf::RenderWindow &);
+    void setPosition(sf::Vector2f);
+    void setScale(sf::Vector2f);
+    sf::FloatRect getGlobalBounds() const;
+    sf::Vector2i getSize() const;
 };
 
 #endif
--- SFML_Tutorial/Projects/Main_Menu/src/GObj.cpp
+++ SFML_Tutorial/Projects/Main_Menu/src/GObj.cpp
@@ -5,3 +5,27 @@ void GObj::draw(sf::RenderWindow &window)
 {
     window.draw(sprite);
 }
+
+void GObj::setPosition(sf::Vector2f position)
+{
+    sprite.setPosition(position);
+}
+
+void GObj::setScale(sf::Vector2f scale)
+{
+    sprite.setScale(scale);
+}
+
+sf::FloatRect GObj::getGlobalBounds() const
+{
+    return sprite.getGlobalBounds();
+}
+
+sf::Vector2i GObj::getSize() const
+{
+    auto bounds = sprite.getGlobalBounds();
+
+    return {
+        static_cast<int>(bounds.size.x),
+        static_cast<int>(bounds.size.y)};
+}
