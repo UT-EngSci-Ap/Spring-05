8a037ee76a1aee6d902c3099cbbed052b8724581
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-21 15:09:49 +0330

    modify resource manager object

---
+++ SFML_Tutorial/Projects/Event_Handling/src/ResourceManager.cpp
@@ -0,0 +1,22 @@
+#include "ResourceManaegr.h"
+
+sf::Texture &ResourceManager::getTexture(const std::string &name)
+{
+    auto it = textures.find(name);
+    if (it != textures.end())
+    {
+        return *(it->second);
+    }
+
+    auto newTexture = std::make_unique<sf::Texture>();
+
+    if (!newTexture->loadFromFile(src_dir + "/" + name))
+    {
+        throw std::runtime_error("Failed to load texture: " + name);
+    }
+
+    sf::Texture &ref = *newTexture;
+    textures[name] = std::move(newTexture);
+
+    return ref;
+}
