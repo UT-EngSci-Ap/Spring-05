25e28010e13e5e7cd2b9b3d9dd9165aaeb7742b4
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-22 15:27:24 +0330

    add font property for ResourceManager

--- SFML_Tutorial/Projects/Ball/src/ResourceManager.cpp
+++ SFML_Tutorial/Projects/Ball/src/ResourceManager.cpp
@@ -1,4 +1,7 @@
-#include "ResourceManaegr.h"
+#include "ResourceManager.h"
+#include <SFML/Graphics.hpp>
+
+ResourceManager::ResourceManager(const std::string &t, const std::string &f) : textures_dir(t), fonts_dir(f) {}
 
 sf::Texture &ResourceManager::getTexture(const std::string &name)
 {
@@ -10,13 +13,33 @@ sf::Texture &ResourceManager::getTexture(const std::string &name)
 
     auto newTexture = std::make_unique<sf::Texture>();
 
-    if (!newTexture->loadFromFile(src_dir + "/" + name))
+    if (!newTexture->loadFromFile(textures_dir + "/" + name))
     {
         throw std::runtime_error("Failed to load texture: " + name);
     }
 
     sf::Texture &ref = *newTexture;
     textures[name] = std::move(newTexture);
+
+    return ref;
+}
+sf::Font &ResourceManager::getFont(const std::string &name)
+{
+    auto it = fonts.find(name);
+    if (it != fonts.end())
+    {
+        return *(it->second);
+    }
+
+    auto newFont = std::make_unique<sf::Font>();
+
+    if (!newFont->openFromFile(fonts_dir + "/" + name))
+    {
+        throw std::runtime_error("Failed to load font: " + name);
+    }
+
+    sf::Font &ref = *newFont;
+    fonts[name] = std::move(newFont);
 
     return ref;
 }
