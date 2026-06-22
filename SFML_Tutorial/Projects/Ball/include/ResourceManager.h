46c395c35b84468a4130a460d6452fa87cc85b64
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-22 15:25:56 +0330

    correct the name of ResourceManager

--- SFML_Tutorial/Projects/Ball/include/ResourceManager.h
+++ SFML_Tutorial/Projects/Ball/include/ResourceManager.h
@@ -8,11 +8,14 @@
 class ResourceManager
 {
 private:
-    std::string src_dir;
+    std::string textures_dir;
+    std::string fonts_dir;
     std::unordered_map<std::string, std::unique_ptr<sf::Texture>> textures;
+    std::unordered_map<std::string, std::unique_ptr<sf::Font>> fonts;
 
 public:
-    ResourceManager(const std::string &s) : src_dir(s) {}
+    ResourceManager(const std::string &t, const std::string &f);
+    sf::Font &getFont(const std::string &);
     sf::Texture &getTexture(const std::string &);
 };
 
