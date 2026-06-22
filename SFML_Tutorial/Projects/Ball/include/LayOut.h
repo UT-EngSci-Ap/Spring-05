fa099e184789e605bed2e446a64e1e0572770634
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-22 15:25:34 +0330

    make LayOut functions more polymorfic

--- SFML_Tutorial/Projects/Ball/include/LayOut.h
+++ SFML_Tutorial/Projects/Ball/include/LayOut.h
@@ -5,9 +5,17 @@
 
 namespace LayOut
 {
-    void centerVertical(std::vector<std::reference_wrapper<GObj>>, sf::Vector2u winSize, float spacing);
+    template <typename SizOPosable>
+    void centerVertical(std::vector<std::reference_wrapper<SizOPosable>>, sf::Vector2u, float spacing = 0);
 
-    void fill(GObj &, sf::Vector2u);
+    template <typename SizOPosable>
+    void centerVertical(std::vector<std::reference_wrapper<SizOPosable>>, sf::Vector2f, float spacing = 0);
+
+    template <typename Scalable>
+    void fill(Scalable &, sf::Vector2f, float percentage = 1);
+
+    template <typename Scalable>
+    void fill(Scalable &, sf::Vector2u, float percentage = 1);
 };
 
 #endif
--- SFML_Tutorial/Projects/Ball/src/LayOut.cpp
+++ SFML_Tutorial/Projects/Ball/src/LayOut.cpp
@@ -1,29 +1,44 @@
 #include "LayOut.h"
 #include "GObj.h"
 
-void LayOut::centerVertical(const std::vector<std::reference_wrapper<GObj>> objects, sf::Vector2u winSize, float spacing)
+template <typename SizOPosable>
+void LayOut::centerVertical(const std::vector<std::reference_wrapper<SizOPosable>> objects, sf::Vector2f winSize, float spacing)
 {
     float totalHeight = 0.f;
     for (auto &object : objects)
     {
-        totalHeight += object.get().getSize().y;
+        totalHeight += object.get().getGlobalBounds().size.y;
     }
     totalHeight += spacing * (objects.size() - 1);
 
     float currentY =
-        (static_cast<float>(winSize.y) - totalHeight) / 2.f;
+        (winSize.y - totalHeight) / 2.f;
 
     for (auto &obj : objects)
     {
-        auto size = obj.get().getSize();
-        obj.get().setPosition({(static_cast<float>(winSize.x) - size.x) / 2.f,
+        auto size = obj.get().getGlobalBounds().size;
+        obj.get().setPosition({(winSize.x - size.x) / 2.f,
                                currentY});
 
         currentY += size.y + spacing;
     }
 }
-void LayOut::fill(GObj &object, sf::Vector2u winSize)
+
+template <typename SizOPosable>
+void LayOut::centerVertical(std::vector<std::reference_wrapper<SizOPosable>> objs, sf::Vector2u b, float spacing)
 {
-    object.setScale({static_cast<float>(winSize.x) / object.getGlobalBounds().size.x,
-                     static_cast<float>(winSize.y) / object.getGlobalBounds().size.y});
+    centerVertical<SizOPosable>(objs, Vector2f(win_size), spacing);
+}
+
+template <typename Scalable>
+void LayOut::fill(Scalable &object, sf::Vector2f winSize, float percentage)
+{
+    object.setScale({winSize.x * percentage / object.getLocalBounds().size.x,
+                     winSize.y * percentage / object.getLocalBounds().size.y});
+}
+
+template <typename Scalable>
+void LayOut::fill(Scalable &object, sf::Vector2u winSize, float percentage)
+{
+    LayOut::fill<Scalable>(object, Vector2f(winSzie), percentage)
 }
