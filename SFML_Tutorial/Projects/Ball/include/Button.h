1500c47f28b172127003e135257b7420b5b824fd
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-22 20:31:01 +0330

    edit button

--- SFML_Tutorial/Projects/Ball/include/Button.h
+++ SFML_Tutorial/Projects/Ball/include/Button.h
@@ -1,20 +1,28 @@
 #ifndef __BUTTON__
 #define __BUTTON__
+
 #include "GObj.h"
 #include <string>
+
 class Button : public GObj
 {
 private:
     sf::Sprite sprite;
     sf::Text text;
 
+    void updateLayout();
+
 public:
-    Button(const sf::Texture &t, const sf::Font &f, sf::Color, const std::string &);
+    Button(sf::Texture &, const sf::Font &, sf::Color, const std::string &);
+
     bool isHovered(const sf::Vector2i &) const;
+
     void draw(sf::RenderWindow &) override;
     void setPosition(sf::Vector2f) override;
     void setScale(sf::Vector2f) override;
+
     sf::FloatRect getGlobalBounds() const override;
+    sf::FloatRect getLocalBounds() const override;
 };
 
 #endif
--- SFML_Tutorial/Projects/Ball/src/Button.cpp
+++ SFML_Tutorial/Projects/Ball/src/Button.cpp
@@ -1,19 +1,21 @@
 #include "Button.h"
 #include "LayOut.h"
-constexpr float TEXT_BUTTON_SCALE = 0.8;
+
+constexpr float TEXT_BUTTON_SCALE = 0.6;
+
+Button::Button(sf::Texture &texture, const sf::Font &f, sf::Color clr, const std::string &str)
+    : sprite(texture), text(f)
+{
+    texture.setSmooth(true);
+    text.setString(str);
+    text.setCharacterSize(10);
+    sprite.setColor(clr);
+    updateLayout();
+}
 
 bool Button::isHovered(const sf::Vector2i &mouse_position) const
 {
     return sprite.getGlobalBounds().contains(sf::Vector2f(mouse_position));
-}
-
-Button::Button(const sf::Texture &t, const sf::Font &f, sf::Color clr, const std::string &str) : sprite(t), text(f)
-{
-    text.setString(str);
-    LayOut::fill(text, sprite.getGlobalBounds().size, TEXT_BUTTON_SCALE);
-    LayOut::centerVertical<sf::Text>(
-        {text},
-        sprite.getGlobalBounds().size);
 }
 
 void Button::draw(sf::RenderWindow &window)
@@ -25,20 +27,28 @@ void Button::draw(sf::RenderWindow &window)
 void Button::setPosition(sf::Vector2f position)
 {
     sprite.setPosition(position);
-    LayOut::centerVertical<sf::Text>(
-        {text},
-        sprite.getGlobalBounds().size);
+    updateLayout();
 }
 
 void Button::setScale(sf::Vector2f scale)
 {
     sprite.setScale(scale);
-    LayOut::fill(
-        text,
-        sprite.getGlobalBounds().size,
-        TEXT_BUTTON_SCALE);
+    updateLayout();
 }
+
 sf::FloatRect Button::getGlobalBounds() const
 {
     return sprite.getGlobalBounds();
+}
+
+sf::FloatRect Button::getLocalBounds() const
+{
+    return sprite.getLocalBounds();
+}
+
+void Button::updateLayout()
+{
+    LayOut::fitText(text, sprite.getGlobalBounds().size, TEXT_BUTTON_SCALE);
+    LayOut::centerVertical<sf::Text>({text}, sprite.getGlobalBounds());
+    text.move({0, -7});
 }
