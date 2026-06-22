103a3f06737bc93245cd7e33c6b4cf7cc176e8f9
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-22 15:24:32 +0330

    modify button class

--- SFML_Tutorial/Projects/Ball/include/Button.h
+++ SFML_Tutorial/Projects/Ball/include/Button.h
@@ -1,12 +1,20 @@
 #ifndef __BUTTON__
 #define __BUTTON__
 #include "GObj.h"
-
+#include <string>
 class Button : public GObj
 {
+private:
+    sf::Sprite sprite;
+    sf::Text text;
+
 public:
-    Button(const sf::Texture &t) : GObj(t) {}
+    Button(const sf::Texture &t, const sf::Font &f, sf::Color, const std::string &);
     bool isHovered(const sf::Vector2i &) const;
+    void draw(sf::RenderWindow &) override;
+    void setPosition(sf::Vector2f) override;
+    void setScale(sf::Vector2f) override;
+    sf::FloatRect getGlobalBounds() const override;
 };
 
 #endif
--- SFML_Tutorial/Projects/Ball/src/Button.cpp
+++ SFML_Tutorial/Projects/Ball/src/Button.cpp
@@ -1,5 +1,44 @@
 #include "Button.h"
+#include "LayOut.h"
+constexpr float TEXT_BUTTON_SCALE = 0.8;
+
 bool Button::isHovered(const sf::Vector2i &mouse_position) const
 {
     return sprite.getGlobalBounds().contains(sf::Vector2f(mouse_position));
+}
+
+Button::Button(const sf::Texture &t, const sf::Font &f, sf::Color clr, const std::string &str) : sprite(t), text(f)
+{
+    text.setString(str);
+    LayOut::fill(text, sprite.getGlobalBounds().size, TEXT_BUTTON_SCALE);
+    LayOut::centerVertical<sf::Text>(
+        {text},
+        sprite.getGlobalBounds().size);
+}
+
+void Button::draw(sf::RenderWindow &window)
+{
+    window.draw(sprite);
+    window.draw(text);
+}
+
+void Button::setPosition(sf::Vector2f position)
+{
+    sprite.setPosition(position);
+    LayOut::centerVertical<sf::Text>(
+        {text},
+        sprite.getGlobalBounds().size);
+}
+
+void Button::setScale(sf::Vector2f scale)
+{
+    sprite.setScale(scale);
+    LayOut::fill(
+        text,
+        sprite.getGlobalBounds().size,
+        TEXT_BUTTON_SCALE);
+}
+sf::FloatRect Button::getGlobalBounds() const
+{
+    return sprite.getGlobalBounds();
 }
