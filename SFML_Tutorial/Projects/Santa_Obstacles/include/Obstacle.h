28a08a2c17e195fa08422f0a3dd58d7d6375c2a6
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-23 20:53:24 +0330

    add collision handling

--- SFML_Tutorial/Projects/Santa_Obstacles/include/Obstacle.h
+++ SFML_Tutorial/Projects/Santa_Obstacles/include/Obstacle.h
@@ -2,11 +2,14 @@
 #define __OBSTACLE__
 #include "Entity.h"
 #include <SFML/Graphics.hpp>
+#include "Direction.h"
+#include "Collision.h"
 
 class Obstacle : public Entity
 {
 private:
     sf::Sprite sprite;
+    Direction calculateSide(sf::FloatRect, sf::FloatRect);
 
 public:
     Obstacle(sf::Texture &);
@@ -16,6 +19,8 @@ public:
     void setScale(sf::Vector2f) override;
     sf::FloatRect getGlobalBounds() const override;
     sf::FloatRect getLocalBounds() const override;
+    sf::Vector2f getPosition() const override;
+    std::optional<Collision> detectCollision(sf::FloatRect);
 };
 
 #endif
--- SFML_Tutorial/Projects/Santa_Obstacles/src/Obstacle.cpp
+++ SFML_Tutorial/Projects/Santa_Obstacles/src/Obstacle.cpp
@@ -4,6 +4,34 @@ Obstacle::Obstacle(sf::Texture &t) : sprite(t)
 {
 }
 
+Direction Obstacle::calculateSide(sf::FloatRect main_rect, sf::FloatRect sub_rect)
+{
+    float overlap_left = (sub_rect.position.x + sub_rect.size.x) - main_rect.position.x;
+    float overlap_right = (main_rect.position.x + main_rect.size.x) - sub_rect.position.x;
+    float overlap_up = (sub_rect.position.y + sub_rect.size.y) - main_rect.position.y;
+    float overlap_down = (main_rect.position.y + main_rect.size.y) - sub_rect.position.y;
+
+    if (overlap_left < overlap_right &&
+        overlap_left < overlap_up &&
+        overlap_left < overlap_down)
+    {
+        return LEFT;
+    }
+    else if (overlap_right < overlap_up &&
+             overlap_right < overlap_down)
+    {
+        return RIGHT;
+    }
+    else if (overlap_up < overlap_down)
+    {
+        return UP;
+    }
+    else
+    {
+        return DOWN;
+    }
+}
+
 void Obstacle::update(float dt) {}
 void Obstacle::draw(sf::RenderWindow &window)
 {
@@ -24,4 +52,19 @@ sf::FloatRect Obstacle::getGlobalBounds() const
 sf::FloatRect Obstacle::getLocalBounds() const
 {
     return sprite.getLocalBounds();
+}
+
+sf::Vector2f Obstacle::getPosition() const
+{
+    return sprite.getPosition();
+}
+
+std::optional<Collision> Obstacle::detectCollision(sf::FloatRect player_bounds)
+{
+    auto obstacle_bounds = sprite.getGlobalBounds();
+    if (const auto intersection = obstacle_bounds.findIntersection(player_bounds))
+    {
+        return Collision{calculateSide(obstacle_bounds, *intersection), *intersection};
+    }
+    return std::nullopt;
 }
