4b8c9d739675d88c9a243578b634abc44db01b80
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-23 20:52:34 +0330

    omit Dir enum and stopVerMove

--- SFML_Tutorial/Projects/Santa_Obstacles/include/Player.h
+++ SFML_Tutorial/Projects/Santa_Obstacles/include/Player.h
@@ -2,13 +2,7 @@
 #define __PLAYER__
 #include "Entity.h"
 #include <SFML/Graphics.hpp>
-
-enum Dir
-{
-    RIGHT,
-    LEFT,
-    NONE
-};
+#include "Direction.h"
 
 class Player : public Entity
 {
@@ -18,7 +12,7 @@ private:
     float jump_speed;
     sf::Vector2f acc;
     sf::Vector2f velocity;
-    Dir dir;
+    Direction dir;
     bool is_jumping;
 
 public:
@@ -35,8 +29,8 @@ public:
     void startMoveLeft();
     void stopMoveRight();
     void stopMoveLeft();
-    void stopVerMove();
     void releaseJump();
+    void stopJump();
 };
 
 #endif
