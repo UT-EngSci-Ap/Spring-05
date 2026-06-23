adffd5b75a3ec7fb90f980a383b0e0a69d9efbcb
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-23 11:01:05 +0330

    implement play state

--- SFML_Tutorial/Projects/Ball/include/Play.h
+++ SFML_Tutorial/Projects/Ball/include/Play.h
@@ -1,14 +1,28 @@
 #ifndef __PLAY__
 #define __PLAY__
 #include "State.h"
+#include "ResourceManager.h"
+#include "Player.h"
+#include "BackGround.h"
+#include "Obstacle.h"
+#include <vector>
+#include <memory>
 
 class Play : public State
 {
 private:
+    BackGround back_ground;
+    Player player;
+    std::vector<std::unique_ptr<Obstacle>> obstacles;
+    void onKeyPressed(const sf::Event::KeyPressed &);
+    void onKeyReleased(const sf::Event::KeyReleased &);
+
 public:
-    Play(sf::Vector2u);
+    Play(ResourceManager &, sf::Vector2u);
     void handleEvent(const sf::Event &) override;
     void draw(sf::RenderWindow &) override;
+    void update(float dt) override;
+    void manageBound();
 };
 
 #endif
